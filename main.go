d6171dbe4e6daa0ce81da6ced7ae9b3265e4db32
Theoo3 <theor17@madronaschool.com>
2020-08-12 21:19:21 -0700

    Inventory System
    - Limited Blocks (just for free at start up)
    - Can Mine Blocks to get them Back
    - All assets are now in the same png, for better performance
    - Can now hold down keys for repeat
    - Font! (its a bad one though)

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 	"image"
 	_ "image/png"
 	"golang.org/x/image/colornames"
-	"strings"
-	"bufio"
+	//"strings"
+	//"bufio"
 	"io/ioutil"
 	"encoding/json"
 )
@@ -35,22 +35,20 @@ type Block struct {
 	breakStage int32
 }
 
-func placeBlock(x int16, y int16, tilePos *[21][21]Block, blockType int32) {
+func placeBlock(x int16, y int16, tilePos *[21][21]Block, inv *[10]ItemStack, selSlot int8) {
 	ok := true
-	if x>19 {
+	if x>19 || x<0 || y>19 || y<0 {
 		ok=false
 	}
-	if x<0 {
+	if tilePos[x][y].btype>0 {
 		ok=false
 	}
-	if y>19 {
-		ok=false
-	}
-	if y<0 {
+	if inv[selSlot].itype==0&&inv[selSlot].amnt==0 {
 		ok=false
 	}
 	if ok {
-		tilePos[x][y].btype = blockType
+		tilePos[x][y].btype = inv[selSlot].itype
+		inv[selSlot].amnt--
 	}
 }
 
@@ -73,16 +71,22 @@ func moveCheck(x int16, y int16, tilePos [21][21]Block) (ok bool) {
 	return true
 }
 
-func deleteBrokenBlocks(tilePos *[21][21]Block, breakThresh int32) {
+func GiveItem(inv *[10]ItemStack, ite ItemStack) {
+	inv[0].itype = ite.itype
+	inv[0].amnt+=ite.amnt
+}
+
+func deleteBrokenBlocks(tilePos *[21][21]Block, inv [10]ItemStack, breakThresh int32) ([10]ItemStack) {
 	for y := 0; y < 20; y++ {
 		for x := 0; x < 20; x++ {
-			if tilePos[x][y].breakStage == breakThresh {
+			if tilePos[x][y].breakStage == breakThresh {	
+				GiveItem(&inv, ItemStack{1, 1})
 				tilePos[x][y].btype = 0
 				tilePos[x][y].breakStage = 0
 			}
 		}
 	}
-	//fmt.Println(tilePos)
+	return inv
 }
 
 func GetExecPath() string {
@@ -125,11 +129,12 @@ func loadConfig(filename string) (config, error) {
 
 func run() {
 
-	loadedConfig, err := loadConfig(GetExecPath()+"/config.json")
+	/*loadedConfig, err := loadConfig(GetExecPath()+"/config.json")
 	if err != nil {
 		panic(err)
 	}
-	reader := bufio.NewReader(os.Stdin)
+
+	/*reader := bufio.NewReader(os.Stdin)
 	fmt.Print("VSync on?(y/n)")
 	ans, _ := reader.ReadString('\n')
 	ans = strings.Replace(ans, "\n", "", -1)
@@ -139,14 +144,12 @@ func run() {
 	}
 	if ans=="y" {
 		sync = true
-	}
-
-
+	}*/
 
 	cfg := pixelgl.WindowConfig{ //the settings for the window
-		Title:  "Online Game",
+		Title:  "Texxit",
 		Bounds: pixel.R(0, 0, 640, 704),
-		VSync:  sync,
+		VSync:  false,
 	}
 
 	win, err := pixelgl.NewWindow(cfg)
@@ -154,65 +157,69 @@ func run() {
 		panic(err)
 	}
 	//load the images from the disc
-	grass, err := LoadPicture("grass.png")	//loading the grass tile
-	tile, err := LoadPicture("wood.png")	//loading the tile tile
-	you, err := LoadPicture("you.png")		//loading you
-	invp, err := LoadPicture("inv.png")	//loading the inv hotbar slors
+	asset, err := LoadPicture("asset.png")	//loading all the assets
 	if err != nil {
 		panic(err)
 	}
-	//make the graphics batches and sprites
-	grasses := pixel.NewBatch(&pixel.TrianglesData{}, grass)
-	grassSpr := pixel.NewSprite(grass, grass.Bounds())
-	tileSpr := pixel.NewSprite(tile, tile.Bounds())
-	youSpr := pixel.NewSprite(you, you.Bounds())
-	invs := pixel.NewBatch(&pixel.TrianglesData{}, invp)
-	invSpr := pixel.NewSprite(invp, invp.Bounds())
+	//make the graphics batches and sprites and stuff
+	num0 := pixel.NewSprite(asset, pixel.R(0, 16, 16, 32))
+	num1 := pixel.NewSprite(asset, pixel.R(16, 16, 32, 32))
+	num2 := pixel.NewSprite(asset, pixel.R(32, 16, 48, 32))
+	num3 := pixel.NewSprite(asset, pixel.R(48, 16, 64, 32))
+	num4 := pixel.NewSprite(asset, pixel.R(64, 16, 80, 32))
+	num5 := pixel.NewSprite(asset, pixel.R(80, 16, 96, 32))
+	num6 := pixel.NewSprite(asset, pixel.R(96, 16, 112, 32))
+	num7 := pixel.NewSprite(asset, pixel.R(112, 16, 128, 32))
+	num8 := pixel.NewSprite(asset, pixel.R(0, 32, 16, 48))
+	num9 := pixel.NewSprite(asset, pixel.R(16, 32, 32, 48))
 
+	woodSpr := pixel.NewSprite(asset, pixel.R(0, 0, 16, 16))
+	youSpr := pixel.NewSprite(asset, pixel.R(16, 0, 32, 16))
+	invSpr := pixel.NewSprite(asset, pixel.R(32, 0, 48, 16))
+	grassSpr := pixel.NewSprite(asset, pixel.R(48, 0, 64, 16))
+	draw := pixel.NewBatch(&pixel.TrianglesData{}, asset)
 	var (
 		player = playerPos{10, 10}
-		frames int = 0
+		frames int16 = 0
 		second = time.Tick(time.Second)
 		selSlot int8
 		tilePos [21][21]Block
 		randAngles = [4]float64{0, 1.5708, 3.14159, 4.71239}
-		inv [52]ItemStack
+		inv [10]ItemStack
+		holdWASD [4]int16
+		grassRotSeed = time.Now().UnixNano()
 	)
-	inv[2] = ItemStack{3, 1}
+	inv[1] = ItemStack{3, 1}
+	inv[4] = ItemStack{85, 1}
+	tilePos[5][5] = Block{1, 99}
 	rand.Seed(time.Now().UnixNano())
 
-	grasses.Clear()
-
-	for x := 16; x != 656; x += 32 {
-		for y := 80; y != 720; y += 32 {
-			grassSpr.Draw(grasses, pixel.IM.Moved(pixel.V(float64(x), float64(y))).ScaledXY(pixel.V(float64(x), float64(y)), pixel.V(2, 2)).Rotated(pixel.V(float64(x), float64(y)), randAngles[rand.Intn(3)]))
-		}
-	}
-
+	draw.Clear()
 	for !win.Closed() {
 		//BEGIN CONTROLS
 		//placing/mining bloks
 		if win.Pressed(pixelgl.KeyUp) {
-			if win.Pressed(pixelgl.KeyLeftShift)&&tilePos[player.x][player.y+1].btype==0 {
-				placeBlock(player.x, player.y+1, &tilePos, 1)
+			if win.Pressed(pixelgl.KeyLeftShift) {
+				placeBlock(player.x, player.y+1, &tilePos, &inv, selSlot)
 			} else {
-				if !win.Pressed(pixelgl.KeyLeftShift)&&tilePos[player.x][player.y+1].btype!=0 {
+				if !win.Pressed(pixelgl.KeyLeftShift) {
 					tilePos[player.x][player.y+1].breakStage++
 				}
 			}
 		}
 		if win.Pressed(pixelgl.KeyDown)&&player.y!=0 {
-			if win.Pressed(pixelgl.KeyLeftShift)&&tilePos[player.x][player.y-1].btype==0 {
-				placeBlock(player.x, player.y-1, &tilePos, 1)
+			if win.Pressed(pixelgl.KeyLeftShift) {
+				placeBlock(player.x, player.y-1, &tilePos, &inv, selSlot)
 			} else {
-				if !win.Pressed(pixelgl.KeyLeftShift)&&tilePos[player.x][player.y-1].btype!=0 {
+				if !win.Pressed(pixelgl.KeyLeftShift) {
 					tilePos[player.x][player.y-1].breakStage++
 				}
 			}
 		}
 		if win.Pressed(pixelgl.KeyRight) {
 			if win.Pressed(pixelgl.KeyLeftShift)&&tilePos[player.x+1][player.y].btype==0 {
-				placeBlock(player.x+1, player.y, &tilePos, 1)
+				fmt.Println()
+				placeBlock(player.x+1, player.y, &tilePos, &inv, selSlot)
 			} else {
 				if !win.Pressed(pixelgl.KeyLeftShift)&&tilePos[player.x+1][player.y].btype!=0 {
 					tilePos[player.x+1][player.y].breakStage++
@@ -221,7 +228,7 @@ func run() {
 		}
 		if win.Pressed(pixelgl.KeyLeft)&&player.x!=0 {
 			if win.Pressed(pixelgl.KeyLeftShift)&&tilePos[player.x-1][player.y].btype==0 {
-				placeBlock(player.x-1, player.y, &tilePos, 1)
+				placeBlock(player.x-1, player.y, &tilePos, &inv, selSlot)
 			} else {
 				if !win.Pressed(pixelgl.KeyLeftShift)&&tilePos[player.x-1][player.y].btype!=0 {
 					tilePos[player.x-1][player.y].breakStage++
@@ -229,7 +236,7 @@ func run() {
 			}
 		}
 
-		deleteBrokenBlocks(&tilePos, 100)
+		inv = deleteBrokenBlocks(&tilePos, inv, 100)
 		//selecting slots
 		if win.JustPressed(pixelgl.Key1) {
 			selSlot = 0
@@ -262,59 +269,186 @@ func run() {
 			selSlot = 9
 		}
 		//moving
+		//up
 		if win.JustPressed(pixelgl.KeyW) {
 			if moveCheck(player.x, player.y+1, tilePos) {
 				player.y++
 			}
 		}
+		if win.Pressed(pixelgl.KeyW) {
+			if holdWASD[0]!=30 {
+				holdWASD[0]++
+			} else {
+				holdWASD[0]-=5
+				if moveCheck(player.x, player.y+1, tilePos) {
+					player.y++
+				}
+			}
+		} else {
+			holdWASD[0] = 0
+		}
+		//left
 		if win.JustPressed(pixelgl.KeyA) {
 			if moveCheck(player.x-1, player.y, tilePos) {
 				player.x--
 			}
 		}
+		if win.Pressed(pixelgl.KeyA) {
+			if holdWASD[1]!=20 {
+				holdWASD[1]++
+			} else {
+				holdWASD[1]-=5
+				if moveCheck(player.x-1, player.y, tilePos) {
+				player.x--
+			}
+			}
+		} else {
+			holdWASD[1] = 0
+		}
+		//down
 		if win.JustPressed(pixelgl.KeyS) {
 			if moveCheck(player.x, player.y-1, tilePos) {
 				player.y--
 			}
 		}
+		if win.Pressed(pixelgl.KeyS) {
+			if holdWASD[2]!=20 {
+				holdWASD[2]++
+			} else {
+				holdWASD[2]-=5
+				if moveCheck(player.x, player.y-1, tilePos) {
+					player.y--
+				}
+			}
+		} else {
+			holdWASD[2] = 0
+		}
+		//right
 		if win.JustPressed(pixelgl.KeyD) {
 			if moveCheck(player.x+1, player.y, tilePos) {
 				player.x++
 			}
 		}
+		if win.Pressed(pixelgl.KeyD) {
+			if holdWASD[3]!=20 {
+				holdWASD[3]++
+			} else {
+				holdWASD[3]-=5
+				if moveCheck(player.x+1, player.y, tilePos) {
+					player.x++
+				}
+			}
+		} else {
+			holdWASD[3] = 0
+		}
 		//END CONTROLS
 		//BEGIN RENDERING
 		win.Clear(colornames.Forestgreen)
-		grasses.Draw(win)
+		draw.Clear()
+
+		rand.Seed(grassRotSeed)
+		for x := 16; x != 656; x += 32 {
+			for y := 80; y != 720; y += 32 {
+				grassSpr.Draw(draw, pixel.IM.Moved(pixel.V(float64(x), float64(y))).ScaledXY(pixel.V(float64(x), float64(y)), pixel.V(2, 2)).Rotated(pixel.V(float64(x), float64(y)), randAngles[rand.Intn(3)]))
+			}
+		}
+
 		for x := 0; x != 20; x++ {
 			for y := 0; y != 20; y++ {
 				if tilePos[x][y].btype>0 {
-					tileSpr.Draw(win, pixel.IM.Moved(pixel.V(float64(x*32)+8, float64(y*32)+40)).ScaledXY(pixel.V(float64(x*32), float64(y*32)), pixel.V(2, 2)))
+					woodSpr.Draw(draw, pixel.IM.Moved(pixel.V(float64(x*32)+8, float64(y*32)+40)).ScaledXY(pixel.V(float64(x*32), float64(y*32)), pixel.V(2, 2)))
 				}
 			}
 		}
-		youSpr.Draw(win, pixel.IM.Moved(pixel.V(float64(player.x*16)+8, float64(player.y*16)+40)).ScaledXY(pixel.V(1, 1), pixel.V(2, 2)))
-		invs.Clear()
+		youSpr.Draw(draw, pixel.IM.Moved(pixel.V(float64(player.x*16)+8, float64(player.y*16)+40)).ScaledXY(pixel.V(1, 1), pixel.V(2, 2)))
 		for x := 32; x != 672; x += 64 {
 			curSlot := (x+32)/64-1
 			if int8(curSlot)==selSlot {
-				invSpr.Draw(invs, pixel.IM.Moved(pixel.V(float64(x), float64(32))).ScaledXY(pixel.V(float64(x), float64(32)), pixel.V(4.6, 4.6)))
+				invSpr.Draw(draw, pixel.IM.Moved(pixel.V(float64(x), float64(32))).ScaledXY(pixel.V(float64(x), float64(32)), pixel.V(4.6, 4.6)))
 			} else {
-				invSpr.Draw(invs, pixel.IM.Moved(pixel.V(float64(x), float64(32))).ScaledXY(pixel.V(float64(x), float64(32)), pixel.V(4, 4)))
+				invSpr.Draw(draw, pixel.IM.Moved(pixel.V(float64(x), float64(32))).ScaledXY(pixel.V(float64(x), float64(32)), pixel.V(4, 4)))
 			}
-			/*switch inv[curSlot].itype {
+		}
+		
+
+		for i := 0; i < 9; i++ {
+			switch inv[i].itype {
 			case 0:
 			case 1:
-				tileSpr.Draw(win, pixel.IM.Moved(pixel.V(float64(x), 32)).ScaledXY(pixel.V(float64(x), 32), pixel.V(2.3, 2.3)))
-				//if inv[curSlot].amnt<1 {
-				//	fmt.Fprintln(basicTxt, inv[curSlot].amnt)
-				//	basicTxt.WriteRune(rune(inv[curSlot].amnt))
-				//}
+				woodSpr.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+32), 32)).ScaledXY(pixel.V(float64((i*64)+32), 32), pixel.V(2.25, 2.25)))
+				if inv[i].amnt<10 {
+					switch inv[i].amnt {
+					case 0:
+						inv[i].itype = 0
+					case 1:
+					case 2:
+						num2.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+50), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 3:
+						num3.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+50), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 4:
+						num4.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+50), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 5:
+						num5.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+50), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 6:
+						num6.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+50), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 7:
+						num7.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+50), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 8:
+						num8.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+50), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 9:
+						num9.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+50), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					}
+				} else { // number i bigger then 10 
+					switch inv[i].amnt / 10 { //most signigf=iashudant digits
+					case 0:
+						num0.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+44), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 1:
+						num1.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+44), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 2:
+						num2.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+44), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 3:
+						num3.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+44), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 4:
+						num4.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+44), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 5:
+						num5.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+44), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 6:
+						num6.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+44), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 7:
+						num7.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+44), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 8:
+						num8.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+44), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 9:
+						num9.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+44), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					}
+
+					switch inv[i].amnt % 10  { // least signifiicant digits
+					case 0:
+						num0.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+56), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 1:
+						num1.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+56), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 2:
+						num2.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+56), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 3:
+						num3.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+56), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 4:
+						num4.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+56), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 5:
+						num5.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+56), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 6:
+						num6.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+56), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 7:
+						num7.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+56), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 8:
+						num8.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+56), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					case 9:
+						num9.Draw(draw, pixel.IM.Moved(pixel.V(float64((i*64)+56), 20)).ScaledXY(pixel.V(float64((i*64)+50), 20), pixel.V(1.25, 1.25)))
+					}
+				}
 			default:
-			}*/
+			}
 		}
-		invs.Draw(win)
-		win.Update()
+		draw.Draw(win)
 		//END RENDERING
 		frames++ //Fps displaying stuff
 		select {
